6f1407279fb3eae29549cb16a9cb6493b279140f
Assaf Koren <assafnoahkoren@gmail.com>
2025-04-05 12:01:21 +0300

    .

--- packages/webapp/src/main.tsx
+++ packages/webapp/src/main.tsx
@@ -4,7 +4,6 @@ import App from './App.tsx';
 
 // Import styles in the correct order
 import '@mantine/core/styles.css';
-import '@unocss/reset/tailwind.css';
 import 'uno.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
