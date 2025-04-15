5a031df359cb3f4e7f2a01782a5264eab2765b09
Assaf Koren <assafnoahkoren@gmail.com>
2025-04-16 00:39:48 +0300

    .

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
-  console.log(`Application is running on: http://localhost:3000`);
+  await app.listen(10000, '0.0.0.0');
+  console.log(`Application is running on: http://0.0.0.0:10000`);
 }
 bootstrap(); 
