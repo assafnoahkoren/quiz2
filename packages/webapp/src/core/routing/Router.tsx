b2a88aa1c3c017c997e31ef2fd0c5427bf947157
Assaf Koren <assafnoahkoren@gmail.com>
2025-04-27 01:19:29 +0300

    .

--- packages/webapp/src/core/routing/Router.tsx
+++ packages/webapp/src/core/routing/Router.tsx
@@ -1,16 +1,13 @@
-import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-const MobileRouter = lazy(() => import('./MobileRouter'));
-const DesktopRouter = lazy(() => import('./DesktopRouter'));
+import MobileRouter from './MobileRouter';
+import DesktopRouter from './DesktopRouter';
 
 export const AppRouter = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/admin/*" element={<DesktopRouter />} />
         <Route path="/*" element={<MobileRouter />} />
       </Routes>
-    </Suspense>
   );
 }; 
