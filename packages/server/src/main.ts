7974f2f71b39a4685bb4950be71d5e8b2b61c96c
Assaf Noah Koren <assafnoahkoren@gmail.com>
2026-05-13 17:19:18 +0300

    feat(server): add global ValidationPipe with transform

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,15 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+      whitelist: true,
+    }),
+  );
   app.enableCors({
-    origin: '*', // Allow all origins
-    methods: '*', // Allow all methods
-    allowedHeaders: '*', // Allow all headers
+    origin: '*',
+    methods: '*',
+    allowedHeaders: '*',
     credentials: true,
     preflightContinue: false,
-    optionsSuccessStatus: 204
+    optionsSuccessStatus: 204,
   });
   await app.listen(10000, '0.0.0.0');
   console.log(`Application is running on: http://0.0.0.0:10000`);
