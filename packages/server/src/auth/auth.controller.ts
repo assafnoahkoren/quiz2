71fb54cb9d844b992e3462f0c2ee0f8ec4beeb32
Assaf Noah Koren <assafnoahkoren@gmail.com>
2026-05-13 17:54:29 +0300

    fix(server): add class-validator decorators to LoginDto and RegisterDto

--- packages/server/src/auth/auth.controller.ts
+++ packages/server/src/auth/auth.controller.ts
@@ -1,16 +1,31 @@
 import { Controller, Post, Body, UseGuards, Req } from '@nestjs/common';
+import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
 import { AuthedRequest } from './types/authed-request.interface';
 
 export class RegisterDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsOptional()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
   password: string;
 }
 
 export class LoginDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
 
